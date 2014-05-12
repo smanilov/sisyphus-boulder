1df7c549c2545132a66c1a3b827b0aeb8eb26d13
Stan Manilov <stanislav.manilov@gmail.com>
2014-05-12 12:22:42 +0100

    Reading input file

--- boulder.py
+++ boulder.py
@@ -5,6 +5,9 @@ import sys
 if len(sys.argv) < 2:
         sys.exit("Usage: %s input_file" % sys.argv[0])
 
-input_file = sys.argv[1]
+input_file_name = sys.argv[1]
+input_file = open(input_file_name)
+text = input_file.read()
 
+print text
 
